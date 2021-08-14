7d60339c5a604aa1442032730256c05313a94b7a
Jason Karjadi <jason@karjadi.com>
2021-08-15 00:47:43 +0900

    before migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("apple");
+import "dotenv-safe/config";
+import path from "path";
+import { createConnection } from "typeorm";
+import { Locale } from "./entities/Locale";
+
+const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [Locale],
+  });
+
+  console.log(conn);
+};
+
+main();
