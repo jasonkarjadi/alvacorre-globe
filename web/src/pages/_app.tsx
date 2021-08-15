621b03f97344666227fd2e7f86a6e9966884b04b
Jason Karjadi <jason@karjadi.com>
2021-08-16 00:00:00 +0900

    reset color mode

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }: any) => {
+  useEffect(() => {
+    localStorage.removeItem("chakra-ui-color-mode");
+  }, []);
+
   return (
     <ChakraProvider resetCSS>
       <Component {...pageProps} />
